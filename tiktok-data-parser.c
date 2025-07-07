e06c129f192b4e20ab8fa808c53f735d5ce0d4ee
commitmaniac <201806046+commitmaniac@users.noreply.github.com>
2025-07-07 04:26:00 +0000

    Use a dynamic array and `realloc` instead

--- tiktok-data-parser.c
+++ tiktok-data-parser.c
@@ -1,22 +1,32 @@
-// SPDX-License-Identifier: BSD-3-Clause
-// SPDX-FileCopyrightText: Copyright (c) 2025 commitmaniac
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+/*
+  SPDX-License-Identifier: BSD-3-Clause
+  SPDX-FileCopyrightText: Copyright (c) 2025 commitmaniac
+*/
 
 #ifdef CJSON_LOCAL
 #include "cJSON.h"
 #else
 #include <cjson/cJSON.h>
-#endif
+#endif /* CJSON_LOCAL */
 
 #define FLAG_IMPLEMENTATION
+#define FLAG_PUSH_DASH_DASH_BACK
 #include "flag.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #ifndef VERSION
 char *VERSION;
-#endif // VERSION
+#endif /* VERSION */
+
+typedef struct {
+    char *items;
+    size_t count;
+    size_t capacity;
+} String_Builder;
 
 typedef struct {
     char *base;
@@ -24,9 +34,34 @@ typedef struct {
     char *result;
 } TikTok_JSON_Keys;
 
-void usage(FILE *stream, const char *prog) {
-    fprintf(stream, "Usage: %s [OPTIONS] FILE\n", prog);
+void flag_program_usage(FILE *stream) {
+    fprintf(stream, "Usage: %s [OPTIONS] PATH\n", flag_program_name());
     flag_print_options(stream);
+}
+
+bool read_entire_file(const char *path, String_Builder *sb) {
+    bool result = true;
+
+    FILE *file = fopen(path, "r");
+    assert(file != NULL);
+    if (fseek(file, 0, SEEK_END) < 0) result = false;
+
+    long fsize = ftell(file);
+    if (fsize < 0) result = false;
+    if (fseek(file, 0, SEEK_SET) < 0) result = false;
+
+    size_t new_count = sb->count + fsize;
+    if (new_count > sb->capacity) {
+        sb->items = realloc(sb->items, new_count);
+        assert(sb->items != NULL);
+        sb->capacity = new_count;
+    }
+
+    fread(sb->items + sb->count, fsize, 1, file);
+    sb->count = new_count;
+
+    if (file) fclose(file);
+    return result;
 }
 
 int main(int argc, char **argv) {
@@ -36,13 +71,13 @@ int main(int argc, char **argv) {
     bool *version = flag_bool("version", false, "Print installed version");
 
     if (!flag_parse(argc, argv)) {
-        usage(stderr, flag_program_name());
+        flag_program_usage(stderr);
         flag_print_error(stderr);
-        exit(1);
+        return 1;
     }
 
     if (*help) {
-        usage(stdout, flag_program_name());
+        flag_program_usage(stderr);
         exit(0);
     }
 
@@ -53,30 +88,20 @@ int main(int argc, char **argv) {
 
     int rest_argc = flag_rest_argc();
     char **rest_argv = flag_rest_argv();
-
     if (rest_argc <= 0) {
         fprintf(stderr, "Error: no input file provided\n");
         exit(1);
     }
 
-    char *filename = rest_argv[0];
-    FILE *file = fopen(filename, "r");
-    assert(file != NULL);
+    char *path = rest_argv[0];
+    String_Builder sb = {0};
 
-    fseek(file, 0, SEEK_END);
-    long f_size = ftell(file);
-    fseek(file, 0, SEEK_SET);
+    if (!read_entire_file(path, &sb)) {
+        fprintf(stderr, "Error: could not read provided input file\n");
+        exit(1);
+    }
 
-    char *buff = malloc(f_size + 1);
-    assert(buff != NULL);
-
-    fread(buff, 1, f_size, file);
-    buff[f_size] = '\0';
-
-    fclose(file);
-
-    cJSON *json = cJSON_Parse(buff);
-    free(buff);
+    cJSON *json = cJSON_Parse(sb.items);
     cJSON *activity = cJSON_GetObjectItem(json, "Activity");
 
     TikTok_JSON_Keys key;
@@ -90,19 +115,18 @@ int main(int argc, char **argv) {
         key.items = "ItemFavoriteList";
         key.result = "link";
     } else {
-        usage(stderr, flag_program_name());
+        flag_program_usage(stderr);
         fprintf(stderr, "Error: no valid option provided\n");
-        cJSON_Delete(json);
         exit(1);
     }
 
     cJSON *base = cJSON_GetObjectItem(activity, key.base);
     cJSON *items = cJSON_GetObjectItem(base, key.items);
-    cJSON *video;
-    cJSON_ArrayForEach(video, items) {
-        cJSON *link = cJSON_GetObjectItem(video, key.result);
-        char *result = cJSON_GetStringValue(link);
-        printf("%s\n", result);
+    cJSON *item;
+    cJSON_ArrayForEach(item, items) {
+        cJSON *result = cJSON_GetObjectItem(item, key.result);
+        char *value = cJSON_GetStringValue(result);
+        printf("%s\n", value);
     }
 
     cJSON_Delete(json);
